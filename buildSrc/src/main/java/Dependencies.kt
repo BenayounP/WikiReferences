030229d1b0c48922ddc81c27ff6fd595ed9242ad
PierreB <pierre@cabnum.fre>
2019-05-07 15:20:45 +0200

    First Commit

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,10 @@
+object Versions {
+    val kotlin = "1.3.30"
+    val appCompat="28.0.0"
+    val constraintLayout="1.1.3"
+}
+object Dependencies {
+    val kotlinStandardLibrary = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}"
+    val appcompatV7 ="com.android.support:appcompat-v7:${Versions.appCompat}"
+    val constraintLayout = "com.android.support.constraint:constraint-layout:${Versions.constraintLayout}"
+}
